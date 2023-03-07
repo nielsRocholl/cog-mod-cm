0d701fb2f3b9b9b5356f34f057746dd910525a49
Niels Rocholl <49755371+nielsRocholl@users.noreply.github.com>
2023-03-07 10:38:29 +0100

    blanc project

---
+++ GraafTel/GraafTel/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  GraafTel
+//
+//  Created by niels rocholl on 07/03/2023.
+//
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GraafTel/GraafTel/GraafTelApp.swift
@@ -0,0 +1,18 @@
+//
+//  GraafTelApp.swift
+//  GraafTel
+//
+//  Created by niels rocholl on 07/03/2023.
+//
+//
+
+import SwiftUI
+
+@main
+struct GraafTelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
