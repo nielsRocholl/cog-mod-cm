79842cf4091e658fb856b3109da96fe72b4258d0
Niels Rocholl <49755371+nielsRocholl@users.noreply.github.com>
2023-03-07 12:26:54 +0100

    added more questions

--- GraafTel/Data.swift
+++ GraafTel/Data.swift
@@ -76,7 +76,46 @@ let dictionary: [String: Any] = [
                 "first_part": "5 dm + 40 cm + 4 mm = ",
                 "answer": "904",
                 "second_part": "mm"
-            ]
+            ],
+            "question_b": [
+                "first_part": "2.1 km + 8 hm + 350 dm = ",
+                "answer": "2935",
+                "second_part": "m"
+            ],
+            "question_c": [
+                "first_part": "3.5 dm + 5 cm + 2.5 dm = ",
+                "answer": "65",
+                "second_part": "cm"
+            ],
         ]
-    ]
+    ],
+    "question_5": [
+        "img": "question_5.png",
+        "question_type": "fill_in_the_blank",
+        "domain": "1.2",
+        "KTI": "application",
+        "question": "On the thermometers you can see the temperature in Kelvin, degrees Celsius, Kelvin and Fahrenheit.\nRead the answers to the questions on the thermometers.",
+        "questions": [
+            "question_a": [
+                "first_part": "What temperature does arrow A indicate in degrees Celsius?",
+                "answer": "25",
+                "second_part": "°C"
+            ],
+            "question_b": [
+                "first_part": "What temperature does arrow B indicate in degrees Fahrenheit?",
+                "answer": "113",
+                "second_part": "°F"
+            ],
+            "question_c": [
+                "first_part": "Arrow C indicates 176 Fahrenheit. What temperature in Kelvin is that?",
+                "answer": "353.15",
+                "second_part": "K"
+            ],
+            "question_d": [
+                "first_part": "What is the difference between the number of degrees Celsius and the temperature in Kelvin?",
+                "answer": "273.15",
+                "second_part": "degrees"
+            ],
+        ]
+    ],
 ]
